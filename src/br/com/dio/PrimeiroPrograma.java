046b99f21630217cf02fc36cfd875a13db5e301c
Silvana <sil.porto09@gmail.com>
2022-07-19 13:57:04 -0300

    "First Commit"

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	Gato gato = new Gato();
+	br.com.dio.Livros Livros = new Livros();
+
+				
+	/*public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+	  System.out.println ("Hello Word!" + (a+b));*/
+
+	}
+
+
+class Livros {
+	private String name;
+	private String npag;
+	
+	
+}
